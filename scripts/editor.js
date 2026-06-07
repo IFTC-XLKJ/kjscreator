2f6f89875c6ca071fc187726abda5532baea768e
IFTC-XLKJ <iftcceo@139.com>
2026-06-07 20:01:46 +0800

    add blockly lib

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,7 +1,17 @@
-// 在 editor.html 的 script 中
 const params = new URLSearchParams(window.location.search);
 const uuid = params.get('uuid');
 const name = params.get('name');
 const path = params.get('path');
 
 console.log("Editor Window Params:", { uuid, name, path });
+
+const { Logger } = iftc;
+
+Logger.info("Editor Window Started");
+// 初始化Blockly
+const workspace = Blockly.inject('blocklyDiv', {
+    toolbox: toolbox,
+    scrollbars: true,
+    trashcan: true,
+    media: 'media/',
+});
