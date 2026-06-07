785d08e774d259499d59ad460d2ce02f12f8c96b
IFTC-XLKJ <iftcceo@139.com>
2026-06-07 16:22:55 +0800

    update index.js,preload.ts

--- preload.ts
+++ preload.ts
@@ -2,6 +2,10 @@ import { contextBridge, ipcRenderer } from "electron";
 import Logger from "./logger.ts";
 import path from "path";
 import { fileURLToPath } from "url";
+// 修复点：删除错误的 import { fs } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+// import * as fs from "fs";
+import os from "os";
 
 interface ChooseDirectoryOptions {
     title?: string;
@@ -21,5 +25,30 @@ contextBridge.exposeInMainWorld("iftc", {
         Logger.info("chooseDirectory done");
         Logger.info("chooseDirectory result: " + result);
         return result;
+    },
+    homeDir: os.homedir().replace(/\\/g, "/"),
+    File: function (filepath: string) {
+        filepath = filepath || os.homedir();
+        filepath = path.resolve(filepath);
+        filepath = filepath.replace(/\\/g, "/");
+        Logger.info("File path: " + filepath);
+        const obj = {
+            path: filepath,
+            read: async () => {
+                Logger.info("start readFile");
+                const result = await readFile(obj.path);
+                Logger.info("readFile done");
+                Logger.info("readFile result: " + result.byteLength + " bytes");
+                return result;
+            },
+            readText: async () => {
+                Logger.info("start readFile");
+                const result = await readFile(obj.path);
+                Logger.info("readFile done");
+                Logger.info("readFile result: " + result.byteLength + " bytes");
+                return await (new Blob([result])).text();
+            },
+        };
+        return obj;
     },
 });
--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,15 @@ async function main() {
     Logger.info("App initialized");
 
     add_project.addEventListener("click", () => {
-        const dialog = Dialog.builder({ headline: '添加项目', view: TemplateHTML.templates.choose_kubejs_dir });
+        const choose_kubejs_dir = TemplateHTML.templates.choose_kubejs_dir;
+        const target_kubejs_dir = choose_kubejs_dir.querySelector("#target_kubejs_dir");
+        target_kubejs_dir.addEventListener("click", async () => {
+            const result = await iftc.chooseDirectory({ title: "请选择KubeJS目录" });
+            Logger.info("chooseDirectory result: " + result);
+            target_kubejs_dir.textContent = result || "未选择";
+            choose_kubejs_dir.dataset.directory = result || "";
+        });
+        const dialog = Dialog.builder({ headline: '添加项目', view: choose_kubejs_dir });
     });
 }
 main();
