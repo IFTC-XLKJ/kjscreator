a6643aa784b069db7b129de2d2b4b380d6bdf93d
IFTC-XLKJ <iftcceo@139.com>
2026-06-07 15:32:18 +0800

    update index.js,index.css,choose_kubejs_dir.html

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,11 @@ const { Dialog } = sober;
 TemplateHTML.baseUrl = "file://" + iftc.__dirname + "/templates/";
 
 async function main() {
+    await TemplateHTML.getTemplate("choose_kubejs_dir");
     Logger.info("App initialized");
 
     add_project.addEventListener("click", () => {
-        const dialog = Dialog.builder({ headline: '添加项目', view:  });
+        const dialog = Dialog.builder({ headline: '添加项目', view: TemplateHTML.templates.choose_kubejs_dir });
     });
 }
 main();
