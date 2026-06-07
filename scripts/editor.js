2276d05ab187f76daa12fff9e4510382f1e5f9cd
IFTC-XLKJ <iftcceo@139.com>
2026-06-07 21:20:32 +0800

    update editor.js;delete blockly.min.js

--- scripts/editor.js
+++ scripts/editor.js
@@ -8,10 +8,37 @@ console.log("Editor Window Params:", { uuid, name, path });
 const { Logger } = iftc;
 
 Logger.info("Editor Window Started");
-// 初始化Blockly
-const workspace = Blockly.inject('blocklyDiv', {
+const workspaceOptions = {
     toolbox: toolbox,
+    collapse: true,
+    css: true,
+    grid: {
+        spacing: 20,
+        length: 3,
+        colour: '#ccc',
+        includeInstructions: false,
+    },
+    modalInputs: false,
+    move: {
+        scrollbars: {
+            horizontal: true,
+            vertical: true,
+        },
+        drag: true,
+        wheel: true,
+    },
     scrollbars: true,
-    trashcan: true,
-    media: 'media/',
-});
+    trashcan: false,
+    renderer: 'zelos',
+    media: 'scripts/blockly/media/',
+    zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 1.0,
+        maxScale: 4,
+        minScale: 0.25,
+        scaleSpeed: 1.1,
+    },
+};
+// 初始化Blockly
+const workspace = Blockly.inject('blocklyDiv', workspaceOptions);
