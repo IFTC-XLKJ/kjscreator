88ca6181fa8e55c796ba3ac4c61ca4ae29afcf63
IFTC-XLKJ <iftcceo@139.com>
2026-06-12 21:16:40 +0800

    update normal.js,field-multilineinput.js,toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -1,11 +1,34 @@
 // Blockly Toolbox
 const toolbox = {
-    kind: "flyoutToolbox",
+    kind: "categoryToolbox",
     contents: [
         {
             kind: "category",
             name: "通用",
-            contents: [],
+            contents: [
+                {
+                    kind: "block",
+                    type: "inline_comment",
+                    input: {
+                        name: "text",
+                        type: "field",
+                        text: "注释"
+                    }
+                },
+                {
+                    kind: "block",
+                    type: "multiline_comment",
+                    input: {
+                        name: "text",
+                        type: "field",
+                        text: "注释"
+                    }
+                },
+                {
+                    kind: "block",
+                    type: "console"
+                },
+            ],
         },
     ]
 };
